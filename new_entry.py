6ab9a59ece3229542c7c11ec7e5b19db46f0720f
Kyle Huang <kyle@anyscale.com>
2024-01-19 09:08:08 -0600

    2024

--- new_entry.py
+++ new_entry.py
@@ -1,17 +1,17 @@
 import argparse
 import os
-PREFIX="23-"
+PREFIX="24-"
 SUFFIX=".md"
 IMG_PRE="assets/images/"
 IMG_SUF="_files/"
 
 ### python new_entry.py -n 12-23-blog-title
-### will generate _posts/23-12-23-blog-title.md
-### and a folder assets/images/23-12-23-blog-title_files
+### will generate _posts/24-12-23-blog-title.md
+### and a folder assets/images/24-12-23-blog-title_files
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-            "-n", "--name", type=str, default="23-00-00-name", help="Test frame name"
+            "-n", "--name", type=str, default="24-00-00-name", help="Test frame name"
     )
     args = parser.parse_args()
     post_name = PREFIX + args.name + SUFFIX
