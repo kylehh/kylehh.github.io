5ccf238d942a4db068f793f37bd3136b96e9ce59
Kyle Huang <yellowsea@gmail.com>
2025-01-24 15:07:09 -0600

    wip

--- new_entry.py
+++ new_entry.py
@@ -1,8 +1,8 @@
 import argparse
 import os
-PREFIX="24-"
+PREFIX="25-"
 SUFFIX=".md"
-IMG_PRE="assets/images/2024/"
+IMG_PRE="assets/images/2025/"
 IMG_SUF="_files/"
 
 ### python new_entry.py -n 12-23-blog-title
@@ -11,11 +11,11 @@ IMG_SUF="_files/"
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-            "-n", "--name", type=str, default="24-00-00-name", help="Test frame name"
+            "-n", "--name", type=str, default="25-00-00-name", help="Test frame name"
     )
     args = parser.parse_args()
     post_name = PREFIX + args.name + SUFFIX
     imgf_name = IMG_PRE + PREFIX + args.name + IMG_SUF
 
-    os.system(f"cp _posts/2024/24-05-05-KAN.md _posts/2024/{post_name}")
+    os.system(f"cp _posts/2024/24-05-05-KAN.md _posts/2025/{post_name}")
     os.mkdir(imgf_name)
